fa5cbc8308f4e9c3e0ef43bc3996e7252bfa25fd
vanStriean <wvstrien@jalem.nl>
2015-03-25 12:37:14 +0100

    test

---
+++ AvansFestival.BeheerUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AvansFestival.BeheerUI.Startup))]
+namespace AvansFestival.BeheerUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ AvansFestivals.UnitTest/UnitTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using AvansFestivals.Domain.Abstract;
+using AvansFestivals.Domain.Database;
+using AvansFestivals.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AvansFestivals.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Mock<IFestivalRepo> mock = new Mock<IFestivalRepo>();
+            mock.Setup(m => m.getQuerableAllFestivals()).Returns(new Festival[] {
+                new Festival {Id = 1, Name = "F1"},
+                new Festival {Id = 2, Name = "F2"},
+                new Festival {Id = 3, Name = "F3"},
+            }.AsQueryable());
+
+            //// Arrange - create a controller
+            //HomeController target = new HomeController(mock.Object);
+
+            //// Action
+            //Festival[] result = ((IEnumerable<Festival>)target.Events().
+            //    ViewData.Model).ToArray();
+
+            //// Assert
+            //Assert.AreEqual(result.Length, 3);
+            //Assert.AreEqual("F1", result[0].Name);
+            //Assert.AreEqual("F2", result[1].Name);
+            //Assert.AreEqual("F3", result[2].Name);
+        }
+    }
+}
